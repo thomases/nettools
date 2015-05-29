79efef11ff5dcdb82425077ce3798f386987e879
Thomas Espe <thomas.espe@gmail.com>
2015-05-29 22:06:13 +0200

    Code cleanup (pep8)

--- oslan.py
+++ oslan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 ##
-# Copyright (C) 2014 by Thomas Hemmingby Espe <te@nilu.no>
+# Copyright (C) 2014, 2015 by Thomas Hemmingby Espe <te@nilu.no>
 #
 
 """
@@ -21,7 +21,8 @@ from textwrap import dedent
 
 Host = namedtuple('Host', 'name ip state os')
 
-def main ():
+
+def main():
     """Main function"""
 
     if (os.geteuid() != 0):
@@ -29,7 +30,7 @@ def main ():
         This program performs OS finger printing via NMAP.
         To do this, root privileges (or sudo) are required.
         ''').strip()
-        return 77 # EX_NOPERM
+        return 77  # EX_NOPERM
 
     opts = parse_args()
     ipn = None
@@ -37,7 +38,7 @@ def main ():
     if not opts.iprange or opts.ip:
         print "Iprange must be provided"
         return 1
-        
+
     if opts.iprange:
         ipn = parse_ip_range(opts.iprange)
         if not ipn:
@@ -45,15 +46,16 @@ def main ():
     else:
         # single ip adresses are not implemented (yet)
         return 1
-    
+
     result = scan(ipn)
     if opts.search:
         mask = re.compile(opts.search, re.I)
-        display( [ lst for lst in result if mask.search(lst.os) ],
-                 opts.full)
+        display([lst for lst in result if mask.search(lst.os)],
+                opts.full)
     else:
         display(result, opts.full)
-        
+
+
 def display(lst, full):
     """Display the result on screen"""
     if full:
@@ -62,12 +64,12 @@ def display(lst, full):
     else:
         for h in lst:
             print "%s: %s" % (h.name, h.os)
-                
+
 
 def scan(subnet):
     """Scan the subnet provided for OS fingerprints"""
     reslist = []
-    
+
     try:
         nm = NM.PortScanner()
     except NM.PortScannerError as nmaperr:
@@ -75,23 +77,25 @@ def scan(subnet):
         Error initializing portscanner object: {}
         Is nmap installed?
         ''').format(str(nmaperr)).strip()
-        sys.exit(69) # nmap (possibly) unavailable
+        sys.exit(69)  # nmap (possibly) unavailable
 
     for ip in subnet.iter_hosts():
         ipstr = str(ip)
         try:
             nm.scan(ipstr, arguments='-O')
         except NM.PortScannerError as scanerr:
-            sys.stderr.write("Caught PortScannerError on ip {}\n".format(ipstr))
+            sys.stderr.write("Caught PortScannerError on ip {}: \n".format(ipstr))
+            sys.stderr.write("{}\n".format(scanerr.msg))
             continue
-            
+
         if nm[ipstr]['status']['state'] != 'up':
             continue
             #if nm[ipstr].hostname() == '':
         try:
             hostnam = socket.gethostbyaddr(ipstr)[0]
         except socket.herror as herr:
-            sys.stderr.write("Unable to look up {}\n".format(ipstr))
+            sys.stderr.write("Unable to look up {}: \n".format(ipstr))
+            sys.stderr.write("{}\n".format(herr.msg))
             hostnam = ipstr
             # hostnam = nm[ipstr].hostname()
         # else:
@@ -101,12 +105,13 @@ def scan(subnet):
                                 ipstr,
                                 nm[ipstr]['status']['state'],
                                 nm[ipstr]['osclass'][0]['osfamily']))
-        except KeyError as kerr:
+        except KeyError as __:
             reslist.append(Host(hostnam,
                                 ipstr,
                                 nm[ipstr]['status']['state'],
                                 'unknown'))
     return reslist
+
 
 def parse_ip_range(iprange):
     """Parse an iprange into a subnet"""
@@ -117,20 +122,20 @@ def parse_ip_range(iprange):
         print "Caught exception" % err.message
         return None
     return subnet
-    
+
+
 def parse_args():
     """Parse options and return the resulting object"""
     parser = argparse.ArgumentParser(description='Find OS of hosts in subnet')
     parser.add_argument('-s', help="String to search for in resulting OS scan", action="store", dest='search')
     parser.add_argument('-F', help="Full listing of result, otherwise hostname and OS is displayed",
                         action="store_true", default=False, dest='full')
-    
+
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-i', help="IP adress (not implemented yet)", action="store", dest='ip')
     group.add_argument('-r', help="IP adress or range", action="store", dest='iprange')
 
     return parser.parse_args()
-
 
 
 if (__name__ == "__main__"):
